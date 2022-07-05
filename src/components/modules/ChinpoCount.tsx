9dcb74868fae20b5ae1004d5f9be939600e47ade
Your Name <you@example.com>
2022-07-05 15:09:29 +0900

    Added Redux Chinpo.

--- src/components/modules/ChinpoCount.tsx
+++ src/components/modules/ChinpoCount.tsx
@@ -6,9 +6,9 @@ function ChinpoCount() {
   const count = useSelector(state => state.chinpoString.count)
   
   return (
-    <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
-      <div><h2>{chinpoString}</h2></div>
-      <div><h2>{count}ちんぽじゃ</h2></div>
+    <div className="text-3xl px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
+      {chinpoString ? <div><h2>{chinpoString}</h2></div> : <div></div>}
+      {count ? <div><h2>{count}ちんぽじゃ</h2></div> : <div></div>}
     </div>
   );
 }
